586be2c087605fc53f20a4d24040f53be3f46900
Katarzyna Kabala <katarzyna_kabala@o2.pl>
2021-12-07 13:49:01 +0100

    1d - add a number of spheres onclick

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
-// ProgressButton({
-//     content: 'Progress Step',
-//     cssClass: 'doughprocess',
-//     enableProgress: true,
-//     isPrimary: true,
-//     duration: 4000,
-//     begin: (args) => {
-//         args.step = 20;
-//     }
-// }, '#progressstep');
-
+var doughmaker = document.querySelector('#dough-maker');
 var i = 0;
+let numberOfSpheres = document.querySelector('#number-sphere');
+let numberOfClicks = 0;
 function move() {
-  if (i == 0) {
+  if (i === 0) {
     i = 1;
     var greenBar = document.querySelector('#green-bar');
-    var doughmaker = document.querySelector('#dough-maker');
 
     var width = 0;
     var id = setInterval(frame, 40);
     function frame() {
         doughmaker.textContent = "Ulep ciasto";
+        greenBar.style.width = "0";
       if (width >= 100) {
         clearInterval(id);
         i = 0;
+        numberOfClicks += 1;
+        numberOfSpheres.textContent = "Liczba ulepionych ciastowych kul: " + numberOfClicks;
+        
       } else {
         width++;
         greenBar.style.width = width + "%";
         doughmaker.textContent = "Zatrzymaj lepienie";
-      }
+      }      
     }
   }
 }
