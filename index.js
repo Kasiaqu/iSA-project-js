169c0889bbe49d77cbb6744fc38efb3dba9f3fb5
Katarzyna Kabala <katarzyna_kabala@o2.pl>
2021-12-10 20:04:03 +0100

    4c - button click

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const progressBar = document.querySelector("#progress");
 const doughmakerButton = document.querySelector("#doughmaker");
 const numberOfSphere = document.querySelector("#number-sphere");
 const flourBag = document.querySelector("#flour-amount");
-const velocity = 0.025;
-const flourAlert = document.querySelector('#flour-alert')
-const tray = document.querySelector('.tray')
-const numberOfCookie = document.querySelector('#number-cookie')
+const velocity = 0.1;
+const flourAlert = document.querySelector('#flour-alert');
+const tray = document.querySelector('.tray');
+const numberOfCookie = document.querySelector('#number-cookie');
+const bakeClick = document.querySelector('#bakeclick');
+const numberOfCookieOnTray = document.querySelector('#number-cookie-on-tray');
 // Preparing application state
 let isForming = false;
 let formingProgress = 0;
@@ -13,8 +15,7 @@ let lastUpdateTime = Date.now();
 let sphereNumber = 0;
 let cookieNumber = 0;
 let flourAmount = 100;
-
-
+let cookieOnTrayNumber = 0;
 // Update screen
 function update() {
   const now = Date.now();
@@ -23,14 +24,10 @@ function update() {
 
   if (isForming === true) {
     // Update distance only if acceleration is on
-
     formingProgress += velocity * elapsedTime;
     progressBar.style.width = formingProgress + "%";
     doughmakerButton.textContent = "Zatrzymaj lepienie";
   }
-
-  
-
   if (formingProgress >= 100) {
     const plate = document.createElement("div");
     plate.classList.add('plate');
@@ -45,8 +42,7 @@ function update() {
     numberOfSphere.textContent = sphereNumber;
     flourBag.textContent = flourAmount -= 10;
     
-
-    yellowBall.addEventListener('click', doughClick);
+    plate.addEventListener('click', doughClick);
 
     let currentWidth = 50;
     let currentHeight = 50;
@@ -59,17 +55,16 @@ function update() {
       yellowBall.style.height = currentHeight + "px";
       cookieNumber++;
       numberOfCookie.textContent = cookieNumber;  
-
+      console.log("cookie Number" + cookieNumber);
       if (currentWidth == 0) {
         plate.remove();
         sphereNumber--;
         numberOfSphere.textContent = sphereNumber;
         return;
       }
-  
     }
 
-    if (flourAmount === 10) {
+    if (flourAmount === 0) {
       flourAlert.textContent = "Za mało mąki!!!";
       return;
     }
@@ -85,6 +80,25 @@ update();
 // Adding event listeners
 doughmakerButton.addEventListener("click", function () {
   isForming = !isForming;
-  doughmakerButton.textContent = "Lep to ciacho ziom";
+  doughmakerButton.textContent = "Lep to ciacho ziom";  
 });
+
+bakeClick.addEventListener("click", function () {
+  if( cookieNumber > 0) { 
+  console.log("cookie Number" + cookieNumber); 
+  cookieOnTrayNumber++;
+  cookieNumber--;
+  
+  if (cookieOnTrayNumber > 9 || cookieNumber < 0){
+    return;
+  }
+  numberOfCookie.textContent = cookieNumber;
+  
+  console.log("cookie On Tray Number" + cookieOnTrayNumber);
+  numberOfCookieOnTray.textContent = cookieOnTrayNumber; 
+  
+}
+
+})
+
 
